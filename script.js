5c04224e64df14b914d1e429f8ddcb4ce2af2d24
yashweybee <yashparmar@weybee.in>
2023-08-11 18:40:01 +0530

    Completed till Coding challenge - 3

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const flights =
 
 const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
 
-const  openingHours =  {
+const openingHours = {
   [weekDays[0]]: {
     open: 12,
     close: 22,
@@ -17,7 +17,7 @@ const  openingHours =  {
     open: 11,
     close: 23,
   },
-  [`Day${1+ 3}`]: {
+  [weekDays[5]]: {
     open: 0, // Open 24 hours
     close: 24,
   },
@@ -29,20 +29,73 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  order (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
 
   },
   openingHours,
-  orderDelivery ({ date, loc, items }) {
+  orderDelivery({ date, loc, items }) {
     console.log(date, loc, items);
   },
-  orderPizza (...items) {
+  orderPizza(...items) {
     console.log(items);
   }
 };
 
+const properties = Object.keys(openingHours);
 
+let openStr = `we are open on ${properties.length}:`
+
+for (const day of properties) {
+  openStr += ` ${day},`
+}
+
+// console.log(openStr);
+// console.log(Object.values(openingHours));
+
+const entries = Object.entries(openingHours);
+
+for (const [key, { open, close }] of entries) {
+  // console.log(x);
+  // console.log(`we are open on ${key} on ${open} and close at ${close}`);
+}
+
+
+
+// console.log(restaurant.openingHours.tue?.open);
+
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
+// for(const x of days){
+//   const open = restaurant.openingHours[x]?.open ?? 'closed'
+//   console.log(`we open on ${x} at ${open}`)
+// }
+
+// console.log(restaurant.order?.(2,2))
+
+// const persons = [{name: "abcd", mail : "udko@ahkh"}]
+
+// console.log(persons[2]?.name ?? 'user noe exitst');
+
+// const one = {
+//   fName : "yuagd",
+//   age: 67,
+//   color: "white"
+// }
+// //  const two = JSON.parse(JSON.stringify(one));
+//  const two = {...one};
+//  two.age = 999
+//  two.fName = "ooooooooooooo"
+//  console.log(one);
+//  console.log(two);
+
+// const one = [ 12,24,356,234,46,234]
+
+// const two = JSON.parse(JSON.stringify(one)); 
+// two[0] = 99999999999
+
+// console.log(one);
+// console.log(two);
 
 
 
@@ -194,23 +247,23 @@ const game = {
   },
 };
 
-const [players1, players2]= game.players
+const [players1, players2] = game.players
 
 // console.log(players1, players2);
 
-const [ gk , ...fieldPlayers] = players1
+const [gk, ...fieldPlayers] = players1
 // console.log(gk, fieldPlayers);
 
 const allPlayers = [...players1, ...players2]
 // console.log(allPlayers);
 
-const playersFinal = ['Thiago', 'Coutinho' ,'Perisic', ...players1]
+const playersFinal = ['Thiago', 'Coutinho', 'Perisic', ...players1]
 // console.log(playersFinal);
 
-const {team1, x : draw, team2} = game.odds
+const { team1, x: draw, team2 } = game.odds
 // console.log(team1, team2 , draw);
 
-function printGoals(...args){
+function printGoals(...args) {
 
   console.log(...args);
 }
@@ -233,3 +286,123 @@ function printGoals(...args){
 // }
 
 // console.log(...players1.entries());
+
+///////////////////////////////////////////////
+
+// Coding Challenge - 2
+
+// const gamePlayers = game.scored;
+
+// for(const [x, player] of gamePlayers.entries()){
+//   console.log(`Goal ${x +1}: ${player}`);
+// }
+
+// // console.log(Object.values(game.odds));
+
+// let avg = 0;
+// let sum = 0
+// for(const x of Object.values(game.odds)){
+//  sum += x;
+//  avg = sum / Object.values(game.odds).length
+// }
+// console.log(avg);
+
+// console.log(Object.entries(game.odds));
+
+// for(const [team, value] of Object.entries(game.odds)){
+//   const str = `${team === 'x' ? 'draw' : 'victory'} ${team === team1 ? 'Bayern Munich:' : 'Borrussia Dortmund:'}`
+
+//   console.log(`Odd of ${str}`, value);
+// }
+
+/////////////////////////////////////////////
+
+// Set
+
+
+// const mySet = new Set([12, 23, 12, 23, 12, 23, 34, 123, 54, 23, 123])
+// console.log(mySet.size);
+// console.log(mySet.has(12));
+// console.log(mySet.has(34));
+// mySet.add(22222)
+// mySet.delete(12)
+// console.log(mySet);
+// // mySet.clear()
+// for (const x of mySet) console.log(x);
+
+// const staff = ["teacher", "manager", "principal", "teacher", "manager"]
+
+// // const uniqueStaff = new Set(staff)
+// const uniqueStaff = [...new Set(staff)]
+// // const [one, two] = [...new Set(staff)]
+// // // console.log(uniqueStaff);
+
+
+// Map
+
+// const myMap = new Map()
+// myMap.set('name', 'Thar')
+// myMap
+//   .set('wheels', 4)
+//   .set('use', 'off roading')
+//   .set('seates', 4)
+//   .set('colors', ['red', 'black', 'white'])
+//   .set(true, 'Available')
+//   .set(false, 'Unavailable')
+// myMap.delete('wheels')
+
+// const arr = [1, 2]
+// myMap.set(arr, "somthing")
+// // console.log(myMap.size);
+// console.log(myMap.get(arr));
+
+
+// const myObj = {
+//   "name": "Yash",
+//   age: 21,
+//   number: 2342312
+// }
+// const newMap = new Map(Object.entries(myObj))
+// console.log(...newMap);
+// console.log(newMap.entries());
+// console.log(newMap.values());
+// console.log(newMap.keys());
+
+
+// for (const x of newMap) {
+//   console.log(x);
+// }
+
+
+/////////////////////////////////////////////
+
+// Coding Challenge - 3
+
+// const gameEvents = new Map([
+//   [17, '⚽ GOAL'],
+//   [36, '🔃 Substitution'],
+//   [47, '⚽ GOAL'],
+//   [61, '🔃 Substitution'],
+//   [64, '🟨 Yellow card'],
+//   [69, '🔴 Red card'],
+//   [70, '🔃 Substitution'],
+//   [72, '🔃 Substitution'],
+//   [76, '⚽ GOAL'],
+//   [80, '⚽ GOAL'],
+//   [92, '🟨 Yellow card'],
+// ]);
+
+// const uniqueEvents = [...new Set(gameEvents.values())]
+// console.log(uniqueEvents);
+
+// gameEvents.delete(64)
+// console.log(gameEvents);
+
+// // console.log(gameEvents.keys());
+
+
+// for (const [key, value] of gameEvents.entries()) {
+//   console.log(key, `${key < 45 ? '[FIRST HALF]' : '[SECOND HALF]'}: ${value}`);
+// }
+
+
